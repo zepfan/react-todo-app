ea1ee6f7633f50599cdbd5e3d4a5747177fb306e
Adam Rich <adm.rich@gmail.com>
2017-03-02 15:12:02 -0800

    Removing constructor method (unused)

--- app/components/TodoItem.js
+++ app/components/TodoItem.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
-	constructor() {
-		super();
-	}
-
+	
 	/** ================ LIFECYCLE =========================== */
 
 	componentDidUpdate() {
